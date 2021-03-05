404958d16ef5ab908d280fa5db8a72b645b98302
Akshay <akshaybabloo@gmail.com>
2021-03-05 20:52:35 +1300

    PKCE verify and challenge added

---
+++ pkce.go
@@ -0,0 +1,46 @@
+// Proof Key for Code Exchange (PKCE) helps public clients to get access tokens and refresh tokens. PKCE is based on
+// RFC7636, more on that can be found at https://tools.ietf.org/html/rfc7636 and https://auth0.com/docs/flows/authorization-code-flow-with-proof-key-for-code-exchange-pkce.
+package pkce
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+)
+
+
+type Pkce struct {
+
+	// Optional RandomString, this will be converted to Pkce.ChallengeCode
+	RandomString string
+
+	// Optional Length of the Pkce.VerifyCode. When RandomString is provided, the Length is ignored.
+	Length       int
+}
+
+// When Pkce.RandomString is provided, VerifyCode is similar to it
+// if not, VerifyCode returns a random string based on Pkce.Length.
+func (p *Pkce) VerifyCode() (string, error) {
+	if p.RandomString == "" {
+		if p.Length == 0 {
+			return "", errors.New("length should be greater than 0")
+		}
+		randomString, err := GenerateRandomString(p.Length)
+		if err != nil {
+			return "", err
+		}
+		p.RandomString = randomString
+		return randomString, nil
+	} else {
+		return p.RandomString, nil
+	}
+}
+
+// ChallengeCode returns a challenge code as mentioned in https://tools.ietf.org/html/rfc7636#section-4.2.
+// The code is based on
+// 	code_challenge = BASE64URL-ENCODE(SHA256(ASCII(code_verifier)))
+func (p *Pkce) ChallengeCode() string {
+	hash := sha256.Sum256([]byte(p.RandomString))
+	toBase64 := base64.RawURLEncoding.EncodeToString(hash[:])
+	return toBase64
+}
---
+++ pkce_test.go
@@ -0,0 +1,95 @@
+package pkce
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPkce_ChallengeCode(t *testing.T) {
+	p := Pkce{
+		RandomString: "45d7820e694481f399e7fb9c444f0cb63301a7254d1401443835d9af2c9a6a5ec5b243c3470feb945336025964ef05c8d2f0e44baf76762ba6136914",
+	}
+
+	assert.Equal(t, "iQoF8w9kq5RnuMdisRXypyOoMCF7FGz-ro7dwHjC28U", p.ChallengeCode())
+}
+
+func TestPkce_VerifyCode(t *testing.T) {
+	p := Pkce{
+		RandomString: "ThisIsARandomString",
+	}
+
+	code, err := p.VerifyCode()
+
+	if assert.Nil(t, err) {
+		assert.Equal(t, "ThisIsARandomString", code)
+	}
+}
+
+func TestPkce_VerifyCode2(t *testing.T) {
+	p := Pkce{
+		Length: 50,
+	}
+
+	code, err := p.VerifyCode()
+
+	if assert.Nil(t, err) {
+		assert.Equal(t, len(code), 50)
+	}
+}
+
+func TestPkce_VerifyCode3(t *testing.T) {
+	p := Pkce{}
+
+	_, err := p.VerifyCode()
+
+	if assert.Error(t, err) {
+		assert.Equal(t, errors.New("length should be greater than 0"), err)
+	}
+}
+
+func ExamplePkce_VerifyCode() {
+	p := Pkce{
+		RandomString: "ThisIsARandomString",
+	}
+
+	code, err := p.VerifyCode()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(code)
+	// Output: ThisIsARandomString
+}
+
+func ExamplePkce_VerifyCode_generatedString() {
+	p := Pkce{
+		Length: 60,
+	}
+
+	code, err := p.VerifyCode()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(code)
+}
+
+func ExamplePkce_ChallengeCode() {
+	p := Pkce{
+		RandomString: "45d7820e694481f399e7fb9c444f0cb63301a7254d1401443835d9af2c9a6a5ec5b243c3470feb945336025964ef05c8d2f0e44baf76762ba6136914",
+	}
+	fmt.Println(p.ChallengeCode())
+	// Output: iQoF8w9kq5RnuMdisRXypyOoMCF7FGz-ro7dwHjC28U
+}
+
+func ExamplePkce_ChallengeCode_generatedString() {
+	p := Pkce{
+		Length: 60,
+	}
+	code, err := p.VerifyCode()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(code, p.ChallengeCode())
+}
